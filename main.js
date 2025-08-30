cf43e95663ee36a99adbe323f5e655a30812fdbe
aitsereal1973 <odidoquinom@hotmail.com>
2025-08-30 00:34:37 -0500

    Add comments for clarity in main.js

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const { app, BrowserWindow } = require('electron');
 
+// Function to create the main application window
 function createWindow() {
   const win = new BrowserWindow({
     width: 800,
@@ -9,17 +10,21 @@ function createWindow() {
     },
   });
 
+  // Load the HTML file into the window
   win.loadFile('index.html');
 }
 
+// When the app is ready, create the window
 app.whenReady().then(createWindow);
 
+// Close the application when all windows are closed, except on macOS
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit();
   }
 });
 
+// Recreate a window in the app when the dock icon is clicked (macOS)
 app.on('activate', () => {
   if (BrowserWindow.getAllWindows().length === 0) {
     createWindow();
